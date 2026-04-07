4f5fc0bef9c4992daea4085848eff62802408262
archita sugandh <architasugandh@gmail.com>
2026-04-07 04:28:25 +0000

    added js, worked on html, project completed

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll('.big-box div');
+// console.log(buttons)
+
+buttons.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+        // console.log(e.target);
+        // console.log(e.currentTarget.id);
+        body.style.backgroundColor = e.target.id;
+    });
+});
